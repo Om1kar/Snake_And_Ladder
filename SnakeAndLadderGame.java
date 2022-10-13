52915ed66260d98449868d86c855b78b183a1664
omkar <omkardudhewar121@gmail.com>
2022-10-13 11:47:27 +0530

    players current position

---
+++ SnakeAndLadderGame.java
@@ -0,0 +1,9 @@
+package SnakeAndLadder;
+
+public class SnakeAndLadderGame {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder Game");
+        int StartingPosition = 0;
+        System.out.println("Current position is= " + StartingPosition);
+    }
+}
