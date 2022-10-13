2f4b326ac57fddc9d63f15e96d30ffcdb50e1981
omkar <omkardudhewar121@gmail.com>
2022-10-13 11:52:42 +0530

    player rolls a dice

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -5,5 +5,8 @@ public class SnakeAndLadderGame {
         System.out.println("Welcome to Snake And Ladder Game");
         int StartingPosition = 0;
         System.out.println("Current position is= " + StartingPosition);
+
+        int RollDice = (int)Math.floor(Math.random()*10)%6+1;
+        System.out.println("player rolls a dice= " +RollDice);
     }
 }
