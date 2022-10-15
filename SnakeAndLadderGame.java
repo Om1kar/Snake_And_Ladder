a254bd92ddb0902087609703fe5783dc7ef1cefa
omkar <omkardudhewar121@gmail.com>
2022-10-15 15:32:04 +0530

    player reaches winning position

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -1,30 +1,46 @@
 package SnakeAndLadder;
 
 public class SnakeAndLadderGame {
+    public static final int NO_PLAY = 1;
+    public static final int LADDER = 2;
+    public static final int SNAKE = 3;
+  public static final int START = 0;
     public static void main(String[] args) {
-        System.out.println("Welcome to Snake And Ladder Game");
         int StartingPosition = 0;
-        System.out.println("Current position is= " + StartingPosition);
+        System.out.println("Welcome to Snake And Ladder Game");
+        System.out.println("Current position is= " + START);
+        while (StartingPosition <= 100) {
 
-        int RollDice = (int)Math.floor(Math.random()*10)%6+1;
-        System.out.println("player rolls a dice= " +RollDice);
+            int RollDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+            System.out.println("player rolls a dice= " + RollDice);
 
+            int Options = (int) Math.floor(Math.random() * 10) % 3;
+            System.out.println("player checking for a option= " + Options);
 
-        int Options = (int)Math.floor(Math.random()*10)%3;
-        System.out.println("player checking for a option= "+ Options);
+            switch (Options) {
+                case NO_PLAY:
+                    System.out.println("Player's new Position is " + StartingPosition);
+                    break;
+                case LADDER:
+                   StartingPosition = StartingPosition + RollDice;
+                    System.out.println("Player's new Position after got LADDER is " + StartingPosition);
+                    break;
 
-        switch (Options){
-            case 1:
-                StartingPosition = RollDice+StartingPosition;
-                System.out.println("player got a Ladder");
-                System.out.println("player's position after got a ladder is "+ RollDice);
-            break;
-            case 2:
-                System.out.println("Player Got a Snake");
-                System.out.println("player's position after got a Snake is "+ StartingPosition);
-            break;
-            default:
-                System.out.println("player Wants to Quit The Game");
+                case SNAKE:
+                    StartingPosition = StartingPosition - RollDice;
+                    if (StartingPosition >= 0)
+                        System.out.println("Player's new Position after got SNAKE is " + StartingPosition);
+                    else
+                        StartingPosition = 0;
+                    System.out.println("Player's new position is " + StartingPosition);
+                    break;
+                default:
+                    System.out.println("Player Wants to Quit The Game");
+            }
         }
+        System.out.println();
+        if (StartingPosition != 100)
+            System.out.println("--------------------------------------------------------------------");
+            System.out.println("Player Won The Game");
     }
 }
